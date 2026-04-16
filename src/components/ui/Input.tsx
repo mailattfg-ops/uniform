e4943e8c7a809996bba59d88955516bf0a4e2934
sj <shijuuk2.98@gmail.com>
2026-04-16 12:45:38 +0530

    padding issue in bottom

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,9 +4,22 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: string;
   icon?: React.ReactNode;
+  allowSpecialCharacters?: boolean;
 }
 
-export const Input: React.FC<InputProps> = ({ label, error, icon, className = '', ...props }) => {
+export const Input: React.FC<InputProps> = ({ label, error, icon, allowSpecialCharacters = false, className = '', ...props }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!allowSpecialCharacters && (props.type === 'text' || props.type === 'tel' || !props.type)) {
+      // Allow alphanumeric, spaces, and basic business symbols (@ . + - , _)
+      // Filter out symbols like ^ & * ( ) % $ # ! ~ ` = { } [ ] | \ : ; " ' < > ? /
+      const sanitized = e.target.value.replace(/[^a-zA-Z0-9\s\@\.\+\-\,\_]/g, '');
+      if (sanitized !== e.target.value) {
+        e.target.value = sanitized;
+      }
+    }
+    props.onChange?.(e);
+  };
+
   return (
     <div className="flex flex-col gap-2 w-full group">
       {label && (
@@ -27,6 +40,7 @@ export const Input: React.FC<InputProps> = ({ label, error, icon, className = ''
             icon ? 'pl-12' : ''
           } ${className}`}
           {...props}
+          onChange={handleChange}
         />
       </div>
       {error && <span className="text-[10px] text-error font-black uppercase tracking-widest ml-1 animate-in shake duration-300">{error}</span>}
